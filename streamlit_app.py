f30a47fd61b72d66854b905c157ec5cac1fe1e08
choies1 <choies1@gmail.com>
2023-01-10 00:08:51 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,8 @@ st.markdown("## 주요 통화")
 a = np.random.rand(100) * 100
 df = pd.DataFrame(a)
 
+plt.rcParams["font.family"] = 'NanumGothic'
+
 ax = df.plot(grid=True, figsize=(15, 5))
 ax.set_title("주가(종가) 그래프", fontsize=30) # 그래프 제목을 지정
 ax.set_xlabel("기간", fontsize=20)             # x축 라벨을 지정
