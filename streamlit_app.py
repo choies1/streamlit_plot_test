1c2b2eb5ad84cde3a921dc75b04c1ecb10106635
choies1 <choies1@gmail.com>
2023-01-10 01:25:03 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,17 +14,15 @@ st.title('Streamlit matplotlib 이용 그래프에서 한글 표시 방법')
 st.write("설치된 나눔 폰트 출력")
 
 temp = subprocess.call(["ls","-al", "/usr/share/fonts/truetype/nanum/"])
-st.write(temp)
+print(temp)
 
-# font_path = '/usr/share/fonts/truetype/nanum/NanumGothicBold.ttf'
+# font_path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
 font_path = '/usr/share/fonts/truetype/nanum/NanumGothicCoding.ttf'
-
-font_name = font_manager.FontProperties(fname=font_path).get_name()
+font_name = fm.FontProperties(fname=font_path).get_name()
 st.write("font_name:", font_name)
-
 # rc('font', family=font_name)
+
 matplotlib.rcParams['font.family'] = font_name
-# matplotlib.rcParams['font.family'] = 'NanumGothic'
 matplotlib.rcParams['axes.unicode_minus'] = False
 
 # 선 그래프 그리기
@@ -35,7 +33,7 @@ df = pd.DataFrame(a)
 # plt.rcParams["font.family"] = 'NanumGothic'
 
 ax = df.plot(grid=True, figsize=(15, 5))
-ax.set_title("주가(종가) 그래프2", fontsize=30) # 그래프 제목을 지정
+ax.set_title("주가(종가) 그래프", fontsize=30) # 그래프 제목을 지정
 ax.set_xlabel("기간", fontsize=20)             # x축 라벨을 지정
 ax.set_ylabel("주가(원)", fontsize=20)         # y축 라벨을 지정
 plt.xticks(fontsize=15)                    # X축 눈금값의 폰트 크기 지정
