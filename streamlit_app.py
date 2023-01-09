c2bc1c16bed1d4bc2aef154de483c222e85b450f
choies1 <choies1@gmail.com>
2023-01-10 00:44:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,10 @@ for font in nanum_font_list:
 temp = subprocess.call(["ls","-al", "/usr/share/fonts/truetype/nanum/"])
 print(temp)
 
-temp = subprocess.call(["cp", "/usr/share/fonts/truetype/nanum/*.*", "/home/appuser/venv/lib/python3.9/site-packages/matplotlib/mpl-data/fonts/ttf/"])
+temp = subprocess.call(["fc-cache","-fv"])
+print(temp)
+
+# temp = subprocess.call(["cp", "/usr/share/fonts/truetype/nanum/*.*", "/home/appuser/venv/lib/python3.9/site-packages/matplotlib/mpl-data/fonts/ttf/"])
 # print(temp)
 
 # usr/share/fonts/truetype/nanum/NanumGothic.ttf
